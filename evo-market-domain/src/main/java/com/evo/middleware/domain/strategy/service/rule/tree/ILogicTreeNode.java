c999644611885ddc431b64bdec40ae8c69fd568f
bread <643061128@qq.com>
2025-03-19 17:24:26 +0800

    基础规则树结构

---
+++ evo-market-domain/src/main/java/com/evo/middleware/domain/strategy/service/rule/tree/ILogicTreeNode.java
@@ -0,0 +1,13 @@
+package com.evo.middleware.domain.strategy.service.rule.tree;
+
+
+import com.evo.middleware.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * 规则树接口
+ */
+public interface ILogicTreeNode {
+
+    DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId);
+
+}
